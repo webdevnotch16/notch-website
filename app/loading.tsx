0831f1778f8720a54bbac23e35a7b4312096f0f8
carrFX <164209128+carrFX@users.noreply.github.com>
2025-11-20 09:46:56 +0700

    feat: add loading

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-3xl lg:text-4xl font-bold text-center">Loading...</h1>
+    </div>
+  )
+}
+
+export default loading
